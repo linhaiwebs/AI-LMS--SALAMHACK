47170daac87bac549f1500ea1bac82de348bb21e
“Eslam“ <aslamalmswdya@gmail.com>
2025-03-19 22:47:09 +0200

    fix event inngest

--- app/inngest/client.js
+++ app/inngest/client.js
@@ -1,4 +1,8 @@
 import { Inngest } from "inngest";
+ 
 
-// Create a client to send and receive events
-export const inngest = new Inngest({ id: "ai-lms" });
+export const inngest = new Inngest({
+ id: "ai-lms",
+  eventKey:
+    "kredD78zfedLFN_5pq6_UwjIOFnAwZW6AArZePX67QiCKUsyUqRt_OlQtnG5L0eI1x1q2HblJKXVD1RzQpg5lQ",
+});
